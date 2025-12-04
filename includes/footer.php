222eb2b4c03e42e1badbeb2ac0ab2956f1719d26
Pham Khanh Linh <khanhling212@gmail.com>
2025-12-04 13:08:34 +0700

    Update footer.php

---
+++ includes/footer.php
@@ -0,0 +1,35 @@
+</main> <footer style="background-color: #3E2723; color: #D7CCC8; padding: 40px 0; margin-top: 50px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 mb-4">
+                <h4 style="font-family: 'Pacifico', cursive; color: #fff;">Bakery House</h4>
+                <p class="small">The best cakes in town. Freshly baked every day with love and passion.</p>
+            </div>
+            <div class="col-md-4 mb-4">
+                <h5 class="text-white">Quick Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="index.php" style="color: #D7CCC8;">Home</a></li>
+                    <li><a href="products.php" style="color: #D7CCC8;">Our Menu</a></li>
+                    <li><a href="contact.php" style="color: #D7CCC8;">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4 mb-4">
+                <h5 class="text-white">Contact Info</h5>
+                <p class="small"><i class="fas fa-map-marker-alt mr-2"></i> 1 Phan Tay Nhac, Tu Liem, Hanoi</p>
+                <p class="small"><i class="fas fa-phone mr-2"></i> +84 8888 888 888</p>
+                <p class="small"><i class="fas fa-envelope mr-2"></i> hello@bakeryhouse.com</p>
+            </div>
+        </div>
+        <hr style="border-color: #5D4037;">
+        <div class="text-center small">
+            &copy; 2025 Bakery House. All rights reserved.
+        </div>
+    </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
+
