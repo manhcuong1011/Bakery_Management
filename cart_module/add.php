7214c8f966088431fb50be73418747979b8651c1
nguyenvaan189 <22070438@vnu.edu.vn>
2025-12-06 23:37:38 +0700

    Update add.php

--- cart_module/add.php
+++ cart_module/add.php
@@ -11,3 +11,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['product_id'])) {
     $product_id = (int) $_POST['product_id'];
     $username = $_SESSION['username'];
     
+ // 1. Lấy user_id
+    $u_res = mysqli_query($con, "SELECT id FROM users WHERE username = '$username'");
+    $user_id = mysqli_fetch_assoc($u_res)['id'];
