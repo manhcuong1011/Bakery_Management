d3b3a13553a62d035506125b9aea1d8cc03f8b35
nguyenvaan189 <22070438@vnu.edu.vn>
2025-12-06 22:41:52 +0700

    Update add.php

--- cart_module/add.php
+++ cart_module/add.php
@@ -4,4 +4,10 @@ session_start();
 require_once '../session.php'; 
 require_once '../db_connect.php';
 
-  
+requireLoggedIn(); // Bắt buộc đăng nhập
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['product_id'])) {
+    
+    $product_id = (int) $_POST['product_id'];
+    $username = $_SESSION['username'];
+    
